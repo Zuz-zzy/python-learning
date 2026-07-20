20347f4bd954b84c6bae3affb3d5e419b001ed11
ZuhaAreej <224125083+Zuz-zzy@users.noreply.github.com>
2026-07-21 03:33:24 +0530

    added variables ex1

---
+++ Python Variables/Exercise_1.py
@@ -0,0 +1,17 @@
+# EXERCISE 1: Python Variables
+# Take a number as input and print its type, then convert it to float and print again.
+
+# Taking input from the user
+input_num = input("Enter a number: ")
+
+# Printing the type of the input number
+print("Type of input number:", type(input_num))
+
+# Converting the input number to float
+float_num = float(input_num)
+
+# Printing the type of the converted float number
+print("Type of converted float number:", type(float_num))
+
+# printing the converted float number
+print("Converted float number:", float_num)
